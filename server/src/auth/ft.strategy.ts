2c93842985ce11d37b3885d1d76164af8ec293e5
rycando <rycando@naver.com>
2021-04-22 20:30:39 +0900

    add ftpassport

--- server/src/auth/ft.strategy.ts
+++ server/src/auth/ft.strategy.ts
@@ -9,7 +9,7 @@ export class FtStrategy extends PassportStrategy(Strategy) {
     super({
       clientID: configService.get('client.id'),
       clientSecret: configService.get('client.secret'),
-      callbackURL: 'http://localhost:3000/user/login/callback',
+      callbackURL: 'http://13.209.202.141/api/user/login/callback',
     });
   }
   async validate(payload: any) {
