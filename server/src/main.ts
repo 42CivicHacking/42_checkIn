d6941a1a08d428daee3ef0bd9a4ae3b8c458d7b8
rycando <rycando@naver.com>
2021-04-22 21:37:39 +0900

    .

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
