5ff85c1eacc206e8ecc0e55da1cd1862346eb170
rycando <rycando@naver.com>
2021-04-22 20:15:44 +0900

    add ftpassport

--- server/src/auth/ft-auth.guard.ts
+++ server/src/auth/ft-auth.guard.ts
@@ -2,4 +2,4 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class FtAuthGuard extends AuthGuard('ft') {}
+export class FtAuthGuard extends AuthGuard('42') {}
