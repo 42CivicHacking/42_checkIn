43e979dfbf2dca1542d70588ec07a1245d1a1287
LeeJuhae <tlatmsdlwngo@naver.com>
2021-04-23 20:21:35 +0900

    fix checkout

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,9 +1,5 @@
 import '../styles/LandingPage.css';
 
-// cookie에  w_auth 있는지 확인
-// 없으면 로그인 버튼 뜨게
-// 있으면 /user/auth => valid check  => 401 : 다시 로그인버튼 / /submit으로
-
 function LandingPage () {
 	const handleLogin = () => {
 		window.location.href = '/api/user/login';
