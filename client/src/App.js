4660d3d5c4eca4db484a9c4dafb93f4b7c5cb65a
LeeJuhae <tlatmsdlwngo@naver.com>
2021-04-23 17:26:18 +0900

    add submit page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-import TempPage from './components/TempPage';
-import CheckInPage from './components/CheckInPage';
+import SubmitPage from './components/SubmitPage';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Route path="/" exact={true} component={LandingPage} />
-      <Route path="/temp" component={TempPage} />
-      <Route path="/checkin" component={CheckInPage}/>
+      <Route path="/submit" component={SubmitPage}/>
     </BrowserRouter>
   );
 }
