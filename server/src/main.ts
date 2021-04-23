09d009bf5451eda0ff9754f5320d28b2c35a36e3
rycando <rycando@naver.com>
2021-04-23 14:12:33 +0900

    fix cors setting

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
