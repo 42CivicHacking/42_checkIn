38d7459a53ad478324e708ff1dae057e53354d38
rycando <rycando@naver.com>
2021-04-24 19:58:37 +0900

    unknown error when using logger

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { logger: ['debug'] });
+  const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
   // app.enableCors({
