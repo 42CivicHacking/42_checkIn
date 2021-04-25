624bd2a7ee46e86c137d4a8a94229332f993defb
rycando <rycando@naver.com>
2021-04-25 16:58:43 +0900

    fix redirection in landingPage & add using info in submitPage

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,8 +1,32 @@
+import { useEffect } from "react";
 import "../styles/LandingPage.css";
 
 function LandingPage() {
+	useEffect(() => {
+		const token = getCookieValue("w_auth");
+		if (token !== "") window.location.href = "/submit";
+	}, []);
+
 	const handleLogin = () => {
 		window.location.href = "/api/user/login";
+	};
+	const getCookieValue = (key) => {
+		let cookieKey = key + "=";
+		let result = "";
+		const cookieArr = document.cookie.split(";");
+
+		for (let i = 0; i < cookieArr.length; i++) {
+			if (cookieArr[i][0] === " ")
+				cookieArr[i] = cookieArr[i].substring(1);
+			if (cookieArr[i].indexOf(cookieKey) === 0) {
+				result = cookieArr[i].slice(
+					cookieKey.length,
+					cookieArr[i].length
+				);
+				return result;
+			}
+		}
+		return result;
 	};
 	return (
 		<>
