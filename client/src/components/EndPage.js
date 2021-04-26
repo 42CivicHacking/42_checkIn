bec716ce837fbf4ab4069b9b66baae509b88e043
rycando <rycando@naver.com>
2021-04-26 15:47:46 +0900

    end page css fixed

--- client/src/components/EndPage.js
+++ client/src/components/EndPage.js
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
-import '../styles/EndPage.css';
+import "../styles/EndPage.css";
 
 function EndPage() {
-	useEffect(()=> {
-		setTimeout(()=>window.location.href='/submit', 3000);
-	})
+	useEffect(() => {
+		setTimeout(() => (window.location.href = "/submit"), 3000);
+	});
 	return (
 		<>
-			<h1>Complete!</h1>
+			<h1 id="endText">Complete!</h1>
 		</>
-	)
+	);
 }
 
 export default EndPage;
