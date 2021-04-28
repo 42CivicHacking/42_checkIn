b17aaf4a2bb4ae2dd5f91d1e0cd8d58b66444ecf
rycando <rycando@naver.com>
2021-04-28 17:46:43 +0900

    modify admin page css

--- client/src/components/AdminPage.js
+++ client/src/components/AdminPage.js
@@ -2,22 +2,23 @@ import React, { useEffect, useRef, useState } from "react";
 import SearchBar from "./SearchBar";
 import * as moment from "moment";
 import axios from "axios";
+import "../styles/AdminPage.css";
 
 function AdminPage() {
 	const [LogType, setLogType] = useState(0);
 	const [Logs, setLogs] = useState([]);
 	const ref = useRef();
 
-	useEffect(async () => {
-		try {
-			const response = await axios.get(`/api/user/status`);
-			if (!(response.data && response.data.isAdmin))
-				window.location.href = "/submit";
-		} catch (err) {
-			console.log(err);
-			window.location.href = "/";
-		}
-	}, []);
+	// useEffect(async () => {
+	// 	try {
+	// 		const response = await axios.get(`/api/user/status`);
+	// 		if (!(response.data && response.data.isAdmin))
+	// 			window.location.href = "/submit";
+	// 	} catch (err) {
+	// 		console.log(err);
+	// 		window.location.href = "/";
+	// 	}
+	// }, []);
 
 	const handleClusterButton = () => {
 		setLogs([]);
@@ -48,59 +49,102 @@ function AdminPage() {
 	};
 
 	return (
-		<div>
-			<div>
-				<button onClick={handleClusterButton}>클러스터 로그</button>
-				<button onClick={handleStudentButton}>학생 로그</button>
-				<button onClick={handleCardButton}>카드 로그</button>
-				<div>
+		<div
+			style={{
+				justifyContent: "center",
+				alignItems: "center",
+				display: "flex",
+				flexDirection: "column",
+			}}
+		>
+			<div className="selectorRapper">
+				<div
+					style={{
+						display: "flex",
+						width: "50%",
+						justifyContent: "center",
+					}}
+				>
+					<button
+						onClick={handleClusterButton}
+						style={{
+							width: "20%",
+							height: "3rem",
+							backgroundColor: "whitesmoke",
+							borderRadius: "0.5rem",
+							border: "1px solid white",
+							margin: "1rem",
+						}}
+					>
+						클러스터 로그
+					</button>
+					<button
+						onClick={handleStudentButton}
+						style={{
+							width: "20%",
+							height: "3rem",
+							backgroundColor: "whitesmoke",
+							borderRadius: "0.5rem",
+							border: "1px solid white",
+							margin: "1rem",
+						}}
+					>
+						학생 로그
+					</button>
+					<button
+						onClick={handleCardButton}
+						style={{
+							width: "20%",
+							height: "3rem",
+							backgroundColor: "whitesmoke",
+							borderRadius: "0.5rem",
+							border: "1px solid white",
+							margin: "1rem",
+						}}
+					>
+						카드 로그
+					</button>
+				</div>
+				<div
+					style={{
+						display: "flex",
+						border: "1px solid whitesmoke",
+						width: "50%",
+						padding: "1rem",
+						height: "5rem",
+					}}
+				>
 					<SearchBar type={LogType} setLogs={setLogs} ref={ref} />
 				</div>
 			</div>
 			<div>
-				<div style={{ display: "flex", flexDirection: "row" }}>
-					<div style={{ margin: "10px", width: "10rem" }}>시간</div>
-					<div style={{ margin: "10px", width: "7rem" }}>출/입</div>
-					<div style={{ margin: "10px", width: "7rem" }}>로그인</div>
-					<div style={{ margin: "10px", width: "5rem" }}>
-						카드 번호
-					</div>
-					<div style={{ margin: "10px", width: "5rem" }}>
-						개포/서초
-					</div>
+				<div className="logRapper">
+					<div className="logBox3">시간</div>
+					<div className="logBox2">출/입</div>
+					<div className="logBox2">로그인</div>
+					<div className="logBox1">카드 번호</div>
+					<div className="logBox1">개포/서초</div>
+					<div className="logBox3">강제 퇴실 처리</div>
 				</div>
 				<hr />
 				{Logs &&
 					Logs.map((log, index) => {
 						const date = new Date(log.createdAt);
 						return (
-							<div
-								key={index}
-								style={{
-									display: "flex",
-									flexDirection: "row",
-								}}
-							>
-								<div style={{ margin: "10px", width: "10rem" }}>
+							<div key={index} className="logRapper">
+								<div className="logBox3">
 									{moment(date).format("MM월 DD일 HH:mm")}
 								</div>
-								<div style={{ margin: "10px", width: "7rem" }}>
-									{log.type}
-								</div>
-								<div style={{ margin: "10px", width: "7rem" }}>
+								<div className="logBox2">{log.type}</div>
+								<div className="logBox2">
 									{log.user.userName}
 								</div>
-								<div style={{ margin: "10px", width: "5rem" }}>
-									{log.card.cardId}
-								</div>
-								<div style={{ margin: "10px", width: "5rem" }}>
+								<div className="logBox1">{log.card.cardId}</div>
+								<div className="logBox1">
 									{log.card.type === 0 ? "개포" : "서초"}
 								</div>
 								<div
-									style={{
-										margin: "10px",
-										width: "10rem",
-									}}
+									className="logBox3"
 									data={log.user.userId}
 									onClick={checkOutOnClick}
 								>
