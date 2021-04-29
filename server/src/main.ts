20a160d63160be795ef5fd1e7a29e1219100213f
rycando <rycando@naver.com>
2021-04-29 17:30:08 +0900

    add swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,13 @@ async function bootstrap() {
     origin: true,
     credentials: true,
   });
+  const config = new DocumentBuilder()
+    .setTitle('42CheckIn')
+    .setDescription('42CheckIn Open API Swagger')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
