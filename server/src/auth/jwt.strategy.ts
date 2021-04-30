de1f976c7c11d6ff771ffb1bc47e0a9d5f3e2388
rycando <rycando@naver.com>
2021-04-30 21:08:33 +0900

    emergency over

--- server/src/auth/jwt.strategy.ts
+++ server/src/auth/jwt.strategy.ts
@@ -18,7 +18,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
   async validate(payload: any) {
-    console.log(payload.sub, payload.username);
     return { _id: payload.sub, name: payload.username };
   }
 }
