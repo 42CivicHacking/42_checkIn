b8183b89c133211f6976822e00e9da8fc3c20fb1
rycando <rycando@naver.com>
2021-05-19 16:23:12 +0900

    add

--- client/src/components/SubmitPage.js
+++ client/src/components/SubmitPage.js
@@ -18,24 +18,22 @@ function CheckInPage() {
 		timeOut: null,
 	});
 
-  const [clusterInfo, setClusterInfo] = useState({
-    gaepo: 0,
-    g_waiting: 0,
-    seocho: 0,
-    s_waiting: 0
-  });
-
+	const [clusterInfo, setClusterInfo] = useState({
+		gaepo: 0,
+		g_waiting: 0,
+		seocho: 0,
+		s_waiting: 0,
+	});
 
 	const [checkAll, setCheckAll] = useState(false);
 	const [checkStatus, setCheckStatus] = useState([false, false, false]);
 	const [readySubmit, setReadySubmit] = useState(false);
 
-  // const [waitingCheckStatus, setWaitingCheckStatus] = useState(false);
-  // const [waitingCluster, setWaitingCluster] = useState(null);
-  // const [readyWait, setReadyWait] = useState(false);
+	// const [waitingCheckStatus, setWaitingCheckStatus] = useState(false);
+	// const [waitingCluster, setWaitingCluster] = useState(null);
+	// const [readyWait, setReadyWait] = useState(false);
 
-  // const [waitStatus, setWaitStatus] = useState('cannot'); // waiting status: cannot, ready, waiting
-
+	// const [waitStatus, setWaitStatus] = useState('cannot'); // waiting status: cannot, ready, waiting
 
 	const { userId, cardNum, waitingNum, status, timeOut } = userInfo;
 	const { gaepo, g_waiting, seocho, s_waiting } = clusterInfo;
@@ -89,38 +87,37 @@ function CheckInPage() {
 		setCheckStatus([isChecked, isChecked, isChecked]);
 	};
 
-  // const handleWait = async () => {
-  //   if (readyWait) {
-  //     try {
-  //       await axios.post(`/api/waiting/create/${waitingCluster === 'gaepo' ? 0 : 1}`);
-  //       try {
-  //         const response = await axios.get('/api/user/status');
-  //         const { user, cluster } = response.data;
-  //         setUserInfo({
-  //           ...userInfo,
-  //           waitingNum: user.waitingNum,
-  //           timeout: user.timeOut
-  //         });
-  //         setClusterInfo({
-  //           gaepo: cluster.gaepo,
-  //           g_waiting: cluster.gaepoWaiting,
-  //           seocho: cluster.seocho,
-  //           s_waiting: cluster.seochoWaiting
-  //         });
-  //         setWaitStatus('waiting');
-  //       } catch (err) {
-  //         console.log(err);
-  //       }
-  //     } catch (err) {
-  //       console.log(err);
-  //       if (err.response.status === 400) {
-  //         const modal = document.getElementById('myModal');
-  //         modal.style.display = 'flex';
-  //       } else console.log(err);
-  //     }
-  //   }
-  // };
-
+	// const handleWait = async () => {
+	//   if (readyWait) {
+	//     try {
+	//       await axios.post(`/api/waiting/create/${waitingCluster === 'gaepo' ? 0 : 1}`);
+	//       try {
+	//         const response = await axios.get('/api/user/status');
+	//         const { user, cluster } = response.data;
+	//         setUserInfo({
+	//           ...userInfo,
+	//           waitingNum: user.waitingNum,
+	//           timeout: user.timeOut
+	//         });
+	//         setClusterInfo({
+	//           gaepo: cluster.gaepo,
+	//           g_waiting: cluster.gaepoWaiting,
+	//           seocho: cluster.seocho,
+	//           s_waiting: cluster.seochoWaiting
+	//         });
+	//         setWaitStatus('waiting');
+	//       } catch (err) {
+	//         console.log(err);
+	//       }
+	//     } catch (err) {
+	//       console.log(err);
+	//       if (err.response.status === 400) {
+	//         const modal = document.getElementById('myModal');
+	//         modal.style.display = 'flex';
+	//       } else console.log(err);
+	//     }
+	//   }
+	// };
 
 	useEffect(() => {
 		const checkSubmitCondition = () => {
@@ -133,112 +130,136 @@ function CheckInPage() {
 			else setReadySubmit(false);
 		};
 
-    // const checkWaitCondition = () => {
-    //   if (waitingCheckStatus === true) setReadyWait(true);
-    //   else setReadyWait(false);
-    // };
+		// const checkWaitCondition = () => {
+		//   if (waitingCheckStatus === true) setReadyWait(true);
+		//   else setReadyWait(false);
+		// };
 
-    const getUserData = async () => {
-      try {
-        const response = await axios.get('/api/user/status');
-        const { user, cluster } = response.data;
-        setUserInfo({
-          userId: user.login,
-          cardNum: user.card,
-          status: user.card !== null ? 'in' : 'out',
-          waitingNum: user.waitingNum,
-          timeOut: user.timeOut
-        });
-        setClusterInfo({
-          gaepo: cluster.gaepo,
-          g_waiting: cluster.gaepoWaiting,
-          seocho: cluster.seocho,
-          s_waiting: cluster.seochoWaiting
-        });
-        // if (cluster.gaepo === 150 && cluster.seocho !== 150) setWaitingCluster('gaepo');
-        // else if (cluster.gaepo !== 150 && cluster.seocho === 150) setWaitingCluster('seocho');
-      } catch (err) {
-        document.cookie = 'w_auth=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-        window.location.href = '/';
-      }
-    };
+		const getUserData = async () => {
+			try {
+				const response = await axios.get("/api/user/status");
+				const { user, cluster } = response.data;
+				setUserInfo({
+					userId: user.login,
+					cardNum: user.card,
+					status: user.card !== null ? "in" : "out",
+					waitingNum: user.waitingNum,
+					timeOut: user.timeOut,
+				});
+				setClusterInfo({
+					gaepo: cluster.gaepo,
+					g_waiting: cluster.gaepoWaiting,
+					seocho: cluster.seocho,
+					s_waiting: cluster.seochoWaiting,
+				});
+				// if (cluster.gaepo === 150 && cluster.seocho !== 150) setWaitingCluster('gaepo');
+				// else if (cluster.gaepo !== 150 && cluster.seocho === 150) setWaitingCluster('seocho');
+			} catch (err) {
+				document.cookie =
+					"w_auth=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+				window.location.href = "/";
+			}
+		};
 
-    const token = getCookieValue('w_auth');
-    if (token !== '') getUserData();
-    // else window.location.href = '/';
-
+		const token = getCookieValue("w_auth");
+		if (token !== "") getUserData();
+		// else window.location.href = '/';
 
 		if (JSON.stringify(checkStatus) !== JSON.stringify([true, true, true]))
 			setCheckAll(false);
 		else setCheckAll(true);
 
-    if (status === 'out') checkSubmitCondition();
-    // if (waitStatus === 'ready') checkWaitCondition();
-    // }, [cardNum, checkStatus, status, userInfo, waitStatus, waitingCheckStatus]);
-  }, [cardNum, checkStatus, status, userInfo]);
+		if (status === "out") checkSubmitCondition();
+		// if (waitStatus === 'ready') checkWaitCondition();
+		// }, [cardNum, checkStatus, status, userInfo, waitStatus, waitingCheckStatus]);
+	}, []);
 
-  return (
-    <div id='page-wrapper'>
-      <div id='checkinout'>
-        <h1 id='title'>
-          {/* {status === 'in' ? '42 CheckOut' : waitStatus !== 'cannot' ? '42 Waiting' : '42 CheckIn'} */}
-          {status === 'in' ? '42 CheckOut' : '42 CheckIn'}
-        </h1>
-        <h4>
-          개포 인원 : {gaepo} / 150 {gaepo === 150 ? `(${g_waiting})` : ''}
-        </h4>
-        <h4>
-          서초 인원 : {seocho} / 150 {seocho === 150 ? `(${s_waiting})` : ''}
-        </h4>
-        <h3> Intra ID : {userId}</h3>
-        {status === 'in' ? (
-          <>
-            <h3>Card Number : {cardNum}</h3>
-            <Button className='submitBtn ready' handleClick={handleCheckOut} text='Check Out' />
-          </>
-        ) : (
-          <div>
-            <div className='input-wrapper' style={{ textAlign: 'left' }}>
-              <label htmlFor='allCheck' style={{ fontSize: '1em' }}>
-                <input id='allCheck' type='checkbox' checked={checkAll} onChange={handleCheckAll} />
-                모두 동의
-              </label>
-              <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <div className='checkbox-wrapper'>
-                  {checkLists.map((checkList, idx) => (
-                    <Checkbox
-                      key={idx}
-                      idx={idx}
-                      text={checkList}
-                      checkStatus={checkStatus}
-                      setCheckStatus={setCheckStatus}
-                    />
-                  ))}
-                </div>
-              </div>
-            </div>
-            <UserInput
-              label='Card Number'
-              type='number'
-              placeholder='카드 번호를 입력해주세요'
-              value={cardNum}
-              handleChange={e => {
-                setUserInfo({
-                  ...userInfo,
-                  cardNum: e.target.value
-                });
-              }}
-            />
-            <Button
-              className={`submitBtn ${readySubmit ? ' ready' : ''}`}
-              handleClick={handleCheckIn}
-              text='Check In'
-            />
-          </div>
-        )}
-      </div>
-      <Modal />
-    </div>
-  );
+	return (
+		<div id="page-wrapper">
+			<div id="checkinout">
+				<h1 id="title">
+					{/* {status === 'in' ? '42 CheckOut' : waitStatus !== 'cannot' ? '42 Waiting' : '42 CheckIn'} */}
+					{status === "in" ? "42 CheckOut" : "42 CheckIn"}
+				</h1>
+				<h4>
+					개포 인원 : {gaepo} / 150{" "}
+					{gaepo === 150 ? `(${g_waiting})` : ""}
+				</h4>
+				<h4>
+					서초 인원 : {seocho} / 150{" "}
+					{seocho === 150 ? `(${s_waiting})` : ""}
+				</h4>
+				<h3> Intra ID : {userId}</h3>
+				{status === "in" ? (
+					<>
+						<h3>Card Number : {cardNum}</h3>
+						<Button
+							className="submitBtn ready"
+							handleClick={handleCheckOut}
+							text="Check Out"
+						/>
+					</>
+				) : (
+					<div>
+						<div
+							className="input-wrapper"
+							style={{ textAlign: "left" }}
+						>
+							<label
+								htmlFor="allCheck"
+								style={{ fontSize: "1em" }}
+							>
+								<input
+									id="allCheck"
+									type="checkbox"
+									checked={checkAll}
+									onChange={handleCheckAll}
+								/>
+								모두 동의
+							</label>
+							<div
+								style={{
+									display: "flex",
+									justifyContent: "center",
+								}}
+							>
+								<div className="checkbox-wrapper">
+									{checkLists.map((checkList, idx) => (
+										<Checkbox
+											key={idx}
+											idx={idx}
+											text={checkList}
+											checkStatus={checkStatus}
+											setCheckStatus={setCheckStatus}
+										/>
+									))}
+								</div>
+							</div>
+						</div>
+						<UserInput
+							label="Card Number"
+							type="number"
+							placeholder="카드 번호를 입력해주세요"
+							value={cardNum}
+							handleChange={(e) => {
+								setUserInfo({
+									...userInfo,
+									cardNum: e.target.value,
+								});
+							}}
+						/>
+						<Button
+							className={`submitBtn ${
+								readySubmit ? " ready" : ""
+							}`}
+							handleClick={handleCheckIn}
+							text="Check In"
+						/>
+					</div>
+				)}
+			</div>
+			<Modal />
+		</div>
+	);
 }
 export default CheckInPage;
