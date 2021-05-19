14e7c859aad0d50bc9483de7202a1b1c8faa73db
LeeJuhae <tlatmsdlwngo@naver.com>
2021-05-19 16:17:29 +0900

    Delete waiting

--- client/src/components/SubmitPage.js
+++ client/src/components/SubmitPage.js
@@ -19,9 +19,9 @@ function CheckInPage() {
   });
 
   const [clusterInfo, setClusterInfo] = useState({
-    gaepo: 150,
+    gaepo: 0,
     g_waiting: 0,
-    seocho: 50,
+    seocho: 0,
     s_waiting: 0
   });
 
@@ -29,11 +29,11 @@ function CheckInPage() {
   const [checkStatus, setCheckStatus] = useState([false, false, false]);
   const [readySubmit, setReadySubmit] = useState(false);
 
-  const [waitingCheckStatus, setWaitingCheckStatus] = useState(false);
-  const [waitingCluster, setWaitingCluster] = useState(null);
-  const [readyWait, setReadyWait] = useState(false);
+  // const [waitingCheckStatus, setWaitingCheckStatus] = useState(false);
+  // const [waitingCluster, setWaitingCluster] = useState(null);
+  // const [readyWait, setReadyWait] = useState(false);
 
-  const [waitStatus, setWaitStatus] = useState('cannot'); // waiting status: cannot, ready, waiting
+  // const [waitStatus, setWaitStatus] = useState('cannot'); // waiting status: cannot, ready, waiting
 
   const { userId, cardNum, waitingNum, status, timeOut } = userInfo;
   const { gaepo, g_waiting, seocho, s_waiting } = clusterInfo;
@@ -85,37 +85,37 @@ function CheckInPage() {
     setCheckStatus([isChecked, isChecked, isChecked]);
   };
 
-  const handleWait = async () => {
-    if (readyWait) {
-      try {
-        await axios.post(`/api/waiting/create/${waitingCluster === 'gaepo' ? 0 : 1}`);
-        try {
-          const response = await axios.get('/api/user/status');
-          const { user, cluster } = response.data;
-          setUserInfo({
-            ...userInfo,
-            waitingNum: user.waitingNum,
-            timeout: user.timeOut
-          });
-          setClusterInfo({
-            gaepo: cluster.gaepo,
-            g_waiting: cluster.gaepoWaiting,
-            seocho: cluster.seocho,
-            s_waiting: cluster.seochoWaiting
-          });
-          setWaitStatus('waiting');
-        } catch (err) {
-          console.log(err);
-        }
-      } catch (err) {
-        console.log(err);
-        if (err.response.status === 400) {
-          const modal = document.getElementById('myModal');
-          modal.style.display = 'flex';
-        } else console.log(err);
-      }
-    }
-  };
+  // const handleWait = async () => {
+  //   if (readyWait) {
+  //     try {
+  //       await axios.post(`/api/waiting/create/${waitingCluster === 'gaepo' ? 0 : 1}`);
+  //       try {
+  //         const response = await axios.get('/api/user/status');
+  //         const { user, cluster } = response.data;
+  //         setUserInfo({
+  //           ...userInfo,
+  //           waitingNum: user.waitingNum,
+  //           timeout: user.timeOut
+  //         });
+  //         setClusterInfo({
+  //           gaepo: cluster.gaepo,
+  //           g_waiting: cluster.gaepoWaiting,
+  //           seocho: cluster.seocho,
+  //           s_waiting: cluster.seochoWaiting
+  //         });
+  //         setWaitStatus('waiting');
+  //       } catch (err) {
+  //         console.log(err);
+  //       }
+  //     } catch (err) {
+  //       console.log(err);
+  //       if (err.response.status === 400) {
+  //         const modal = document.getElementById('myModal');
+  //         modal.style.display = 'flex';
+  //       } else console.log(err);
+  //     }
+  //   }
+  // };
 
   useEffect(() => {
     const checkSubmitCondition = () => {
@@ -124,10 +124,10 @@ function CheckInPage() {
       else setReadySubmit(false);
     };
 
-    const checkWaitCondition = () => {
-      if (waitingCheckStatus === true) setReadyWait(true);
-      else setReadyWait(false);
-    };
+    // const checkWaitCondition = () => {
+    //   if (waitingCheckStatus === true) setReadyWait(true);
+    //   else setReadyWait(false);
+    // };
 
     const getUserData = async () => {
       try {
@@ -146,8 +146,8 @@ function CheckInPage() {
           seocho: cluster.seocho,
           s_waiting: cluster.seochoWaiting
         });
-        if (cluster.gaepo === 150 && cluster.seocho !== 150) setWaitingCluster('gaepo');
-        else if (cluster.gaepo !== 150 && cluster.seocho === 150) setWaitingCluster('seocho');
+        // if (cluster.gaepo === 150 && cluster.seocho !== 150) setWaitingCluster('gaepo');
+        // else if (cluster.gaepo !== 150 && cluster.seocho === 150) setWaitingCluster('seocho');
       } catch (err) {
         document.cookie = 'w_auth=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
         window.location.href = '/';
@@ -156,20 +156,22 @@ function CheckInPage() {
 
     const token = getCookieValue('w_auth');
     if (token !== '') getUserData();
-    else window.location.href = '/';
+    // else window.location.href = '/';
 
     if (JSON.stringify(checkStatus) !== JSON.stringify([true, true, true])) setCheckAll(false);
     else setCheckAll(true);
 
     if (status === 'out') checkSubmitCondition();
-    if (waitStatus === 'ready') checkWaitCondition();
-  }, [cardNum, checkStatus, status, userInfo, waitStatus, waitingCheckStatus]);
+    // if (waitStatus === 'ready') checkWaitCondition();
+    // }, [cardNum, checkStatus, status, userInfo, waitStatus, waitingCheckStatus]);
+  }, [cardNum, checkStatus, status, userInfo]);
 
   return (
     <div id='page-wrapper'>
       <div id='checkinout'>
         <h1 id='title'>
-          {status === 'in' ? '42 CheckOut' : waitStatus !== 'cannot' ? '42 Waiting' : '42 CheckIn'}
+          {/* {status === 'in' ? '42 CheckOut' : waitStatus !== 'cannot' ? '42 Waiting' : '42 CheckIn'} */}
+          {status === 'in' ? '42 CheckOut' : '42 CheckIn'}
         </h1>
         <h4>
           개포 인원 : {gaepo} / 150 {gaepo === 150 ? `(${g_waiting})` : ''}
@@ -182,92 +184,6 @@ function CheckInPage() {
           <>
             <h3>Card Number : {cardNum}</h3>
             <Button className='submitBtn ready' handleClick={handleCheckOut} text='Check Out' />
-          </>
-        ) : waitStatus === 'ready' || waitStatus === 'waiting' ? (
-          <>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-              <div className='checkbox-wrapper'>
-                {waitingNoti.map((element, id) => (
-                  <label
-                    key={id}
-                    htmlFor={id}
-                    className='checkbox-text'
-                    style={{ wordBreak: 'keep-all' }}
-                  >
-                    <input
-                      id={id}
-                      className='checkbox'
-                      type='checkbox'
-                      checked={waitingCheckStatus}
-                      onChange={() => {
-                        setWaitingCheckStatus(!waitingCheckStatus);
-                      }}
-                    />
-                    {element}
-                  </label>
-                ))}
-              </div>
-            </div>
-            {waitStatus === 'waiting' ? (
-              <>
-                <h3>Waiting Number: {waitingNum}</h3>
-                {timeOut !== null ? <Timer mm={10} ss={0} /> : null}
-                <Button
-                  className={`submitBtn ${timeOut !== null ? ' ready' : ''}`}
-                  handleClick={() => {
-                    if (timeOut !== null) setWaitStatus('cannot');
-                  }}
-                  text='Go to Check In'
-                />
-                <Button
-                  className='submitBtn ready'
-                  handleClick={() => {
-                    if (window.confirm('대기 취소하시겠습니까?')) {
-                      setWaitStatus('cannot');
-                      // 대기 취소 logic 💡
-                    }
-                  }}
-                  text='Cancel Waiting'
-                />
-              </>
-            ) : (
-              <>
-                <div style={{ margin: '1rem' }}>
-                  <h3>Cluster</h3>
-                  {gaepo === 150 ? (
-                    <label>
-                      <input
-                        type='radio'
-                        name='cluster'
-                        defaultChecked={seocho !== 150}
-                        onChange={() => {
-                          setWaitingCluster('gaepo');
-                        }}
-                      />
-                      개포
-                    </label>
-                  ) : null}
-                  {seocho === 150 ? (
-                    <label>
-                      <input
-                        type='radio'
-                        name='cluster'
-                        defaultChecked={gaepo !== 150}
-                        onChange={() => {
-                          setWaitingCluster('seocho');
-                        }}
-                      />
-                      서초
-                    </label>
-                  ) : null}
-                </div>
-                <Button
-                  className={`submitBtn ${readyWait ? ' ready' : ''}`}
-                  handleClick={handleWait}
-                  text='Want To Wait'
-                />
-              </>
-            )}
           </>
         ) : (
           <div>
@@ -307,15 +223,6 @@ function CheckInPage() {
               handleClick={handleCheckIn}
               text='Check In'
             />
-            {gaepo === 150 || seocho === 150 ? (
-              <Button
-                className='submitBtn ready'
-                handleClick={() => {
-                  setWaitStatus('ready');
-                }}
-                text='Want To Wait'
-              />
-            ) : null}
           </div>
         )}
       </div>
